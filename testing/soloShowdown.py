5a7f749aa009c139fda60cd1eb9ca9ef2469b43b
Modus <mauricehess@hotmail.de>
2023-02-09 15:20:19 +0100

    added some comments

--- testing/soloShowdown.py
+++ testing/soloShowdown.py
@@ -9,10 +9,10 @@ def get_from_battle(i, value, response):
 def main(response, player, i):
     battle_time = response["items"][i]["battleTime"]
     battle_time = battle_time[0:4] + "-" + battle_time[4:6] + "-" + battle_time[6:8] + " " + battle_time[9:11] + ":" + battle_time[11:13] + ":" + battle_time[13:]
-    timestamps = testing.googlesheet.get_timestamps(player)
+    timestamps = testing.googlesheet.get_timestamps(player) # gets all timestamps of stored battles
     bt = str(battle_time)
     bt = [bt]
-    if not(bt in timestamps):
+    if not(bt in timestamps): # checks if battle is already in excel file
         battle_id = response["items"][i]["event"]["id"]
         battle_mode = get_from_battle(i, "mode", response)
         battle_type = get_from_battle(i, "type", response)
